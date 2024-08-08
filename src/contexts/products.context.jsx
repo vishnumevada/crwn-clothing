938cab264ff1e0d84c6aa77b1668affaa3f9fb64
vishnu mevada <vishnu.mevada@txtech.co>
2024-08-08 18:10:07 +0530

    added category preview and functionality

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,13 +1,28 @@
-import { createContext, useState } from "react";
-
-import PRODUCTS from "../shop-data.json";
+import { createContext, useState, useEffect } from "react";
+import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils";
+// import SHOP_DATA from "../shop-data.js";
 
 export const ProductsContext = createContext({
     products: [],
 });
 
 export const ProductsProvider = ({ children }) => {
-    const [products, setProducts] = useState(PRODUCTS);
+    const [products, setProducts] = useState([]);
+
+    // add shop data to firestore
+    // useEffect(() => {
+    //     addCollectionAndDocuments("categories", SHOP_DATA);
+    // }, []);
+
+    useEffect(() => {
+        const getCategoriesMap = async () => {
+            const categoryMap = await getCategoriesAndDocuments();
+            console.log(categoryMap);
+        };
+
+        getCategoriesMap();
+    }, []);
+
     const value = { products };
     return (
         <ProductsContext.Provider value={value}>
