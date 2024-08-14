07530bffde2dd8b86414746023fb4fbea3667df2
vishnu mevada <vishnu.mevada@txtech.co>
2024-08-14 16:08:47 +0530

    Integrated stripe payment

--- src/components/button/button.styles.jsx
+++ src/components/button/button.styles.jsx
@@ -1,13 +1,13 @@
 import styled from "styled-components";
 
 export const BaseButton = styled.button`
-    min-width: 120px;
+    min-width: 165px;
     width: auto;
-    height: 40px;
+    height: 50px;
     letter-spacing: 0.5px;
-    line-height: 40px;
-    padding: 0 20px;
-    font-size: 14px;
+    line-height: 50px;
+    padding: 0 35px 0 35px;
+    font-size: 15px;
     background-color: black;
     color: white;
     text-transform: uppercase;
@@ -17,27 +17,12 @@ export const BaseButton = styled.button`
     cursor: pointer;
     display: flex;
     justify-content: center;
+    align-items: center;
 
     &:hover {
         background-color: white;
         color: black;
         border: 1px solid black;
-    }
-
-    @media (min-width: 768px) {
-        min-width: 140px;
-        height: 45px;
-        line-height: 45px;
-        padding: 0 25px;
-        font-size: 15px;
-    }
-
-    @media (min-width: 1024px) {
-        min-width: 165px;
-        height: 50px;
-        line-height: 50px;
-        padding: 0 30px;
-        font-size: 15px;
     }
 `;
 
@@ -60,5 +45,26 @@ export const InvertedButton = styled(BaseButton)`
         background-color: black;
         color: white;
         border: none;
+    }
+`;
+
+export const LoadingSpinner = styled.div`
+    display: inline-block;
+    width: 30px;
+    height: 30px;
+    border: 3px solid rgba(195, 195, 195, 0.6);
+    border-radius: 50%;
+    border-top-color: #636767;
+    animation: spin 1s ease-in-out infinite;
+    -webkit-animation: spin 1s ease-in-out infinite;
+    @keyframes spin {
+        to {
+            -webkit-transform: rotate(360deg);
+        }
+    }
+    @-webkit-keyframes spin {
+        to {
+            -webkit-transform: rotate(360deg);
+        }
     }
 `;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
+import { Elements } from "@stripe/react-stripe-js";
+
 import { store, persistor } from "./store/store";
+import { stripePromise } from "./utils/stripe/stripe.utils";
 import reportWebVitals from "./reportWebVitals";
 import App from "./App";
 import "./index.scss";
@@ -14,7 +17,9 @@ root.render(
         <Provider store={store}>
             <PersistGate loading={null} persistor={persistor}>
                 <BrowserRouter>
-                    <App />
+                    <Elements stripe={stripePromise}>
+                        <App />
+                    </Elements>
                 </BrowserRouter>
             </PersistGate>
         </Provider>
